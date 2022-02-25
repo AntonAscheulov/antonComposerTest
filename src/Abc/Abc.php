347e2ac8b30763f626c9a48763ef85a9c40203dc
AntonAscheulov <schuca120@mail.ru>
2022-02-25 21:15:42 +0300

    test package

---
+++ src/Abc/Abc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Antonascheulov\Packagetest\Abc;
+
+class Abc
+{
+public static function hello (){
+
+    echo "Hello Im Abc!";
+}
+}
---
+++ src/Bca/Bca.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Antonascheulov\Packagetest\Abc;
+
+class Bca
+{
+    public static function hello()
+    {
+
+        echo "Hello Im Bca!";
+    }
+}
